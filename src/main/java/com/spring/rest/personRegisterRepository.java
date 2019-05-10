1e709cea752f1ccc3aded56d2c9495655e48116d
Daniel Bandeira <daniel@stayon.tech>
2019-05-10 15:51:38 -0300

    Changed personRegister respository package

---
+++ src/main/java/com/spring/rest/personRegisterRepository.java
@@ -0,0 +1,6 @@
+package com.spring.rest;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface personRegisterRepository extends JpaRepository<personRegister, Long>{
+
+}
