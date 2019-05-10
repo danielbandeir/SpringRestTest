d765a2c5947e796c51f66de557f1a19434bd2149
Daniel Bandeira <daniel@stayon.tech>
2019-05-10 15:50:43 -0300

    Changed person register package

---
+++ src/main/java/com/spring/rest/personRegister.java
@@ -0,0 +1,25 @@
+package com.spring.rest;
+
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+
+@Data
+@Entity
+public class personRegister{
+	
+	@Id @GeneratedValue 
+	private Long id;
+	private String name;
+	private String lastName;
+	
+	public personRegister(){}
+	
+	public personRegister(String name, String lastName){
+		this.name = name;
+		this.lastName = lastName;
+	}
+}
