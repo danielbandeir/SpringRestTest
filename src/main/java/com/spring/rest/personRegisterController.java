2f11114bfec5c8ba9f4bb06504e65381036e6758
Daniel Bandeira <daniel@stayon.tech>
2019-05-10 15:51:16 -0300

    Changed controller package to com.spring.rest

---
+++ src/main/java/com/spring/rest/personRegisterController.java
@@ -0,0 +1,31 @@
+package com.spring.rest;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@RestController
+@RequestMapping("/person")
+class personRegisterController {
+	
+	private final personRegisterRepository repository;
+	
+	personRegisterController(personRegisterRepository repository){
+		this.repository = repository;
+	}
+	
+	@GetMapping()
+	List<personRegister> all(){
+		return repository.findAll();
+	}
+	
+	@PostMapping()
+	personRegister newPerson(@RequestBody personRegister newPerson) {
+		return repository.save(newPerson);
+	}
+
+}
