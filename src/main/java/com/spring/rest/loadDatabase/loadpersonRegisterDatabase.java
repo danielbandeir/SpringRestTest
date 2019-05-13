bf231787cd933b38eafef7ab90c60534f3008a4e
Daniel Bandeira <daniel@stayon.tech>
2019-05-13 11:49:13 -0300

    Changed database to load

--- src/main/java/com/spring/rest/loadDatabase/loadpersonRegisterDatabase.java
+++ src/main/java/com/spring/rest/loadDatabase/loadpersonRegisterDatabase.java
@@ -1,10 +1,13 @@
-package com.spring.rest;
+package com.spring.rest.loadDatabase;
 
 import lombok.extern.slf4j.Slf4j;
 
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import com.spring.rest.models.personRegister;
+import com.spring.rest.repository.personRegisterRepository;
 
 
 @Configuration
