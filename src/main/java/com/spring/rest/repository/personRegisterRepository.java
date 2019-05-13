b45eb994e0596f601a8153d144c59eff3821191d
Daniel Bandeira <daniel@stayon.tech>
2019-05-13 11:50:12 -0300

    Added person register repository

--- src/main/java/com/spring/rest/repository/personRegisterRepository.java
+++ src/main/java/com/spring/rest/repository/personRegisterRepository.java
@@ -1,5 +1,7 @@
-package com.spring.rest;
+package com.spring.rest.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.spring.rest.models.personRegister;
 
 public interface personRegisterRepository extends JpaRepository<personRegister, Long>{
 
