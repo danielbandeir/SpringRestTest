b3db3e7e13f69f2548d26f97d53e8c62cb37111b
Daniel Bandeira <daniel@stayon.tech>
2019-05-14 15:54:46 -0300

    Added skin repository

---
+++ src/main/java/com/spring/rest/repository/skinRepository.java
@@ -0,0 +1,8 @@
+package com.spring.rest.repository;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.spring.rest.models.skin;
+
+public interface skinRepository extends JpaRepository<skin, Long>{
+
+}
