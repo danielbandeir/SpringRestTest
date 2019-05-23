77ef3c0fc9025af1dd8afc40c219fd388540eebb
Daniel Bandeira <daniel@stayon.tech>
2019-05-23 15:59:48 -0300

    Updated skinController

--- src/main/java/com/spring/rest/controllers/skinController.java
+++ src/main/java/com/spring/rest/controllers/skinController.java
@@ -1,10 +1,13 @@
 package com.spring.rest.controllers;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import com.spring.rest.models.skin;
 import com.spring.rest.repository.skinRepository;
@@ -32,5 +35,9 @@ class skinController {
 		return repository.save(newSkin);
 	}
 	
+	@PutMapping("/skin/{id}")
+	Optional<skin> replaceSkin(@RequestBody skin newSkin, @PathVariable Long id){
+		return repository.findById(id);
+	}
 }
 
