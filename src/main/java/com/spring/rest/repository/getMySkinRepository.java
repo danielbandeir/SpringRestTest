4e5db34596a9e5332c29deaac1231bb8d51768d4
Daniel Bandeira <daniel@stayon.tech>
2019-05-23 11:25:28 -0300

    Added getMySkin repository

---
+++ src/main/java/com/spring/rest/repository/getMySkinRepository.java
@@ -0,0 +1,9 @@
+package com.spring.rest.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.spring.rest.models.getMySkin;
+
+public interface getMySkinRepository extends JpaRepository<getMySkin, Long> {
+
+}
